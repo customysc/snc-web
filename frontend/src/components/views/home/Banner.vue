537586885d836fc0242fbcb8a864dbba1bb4a67f
ltlan256 <2159376435@qq.com>
2026-01-27 17:08:47 +0800

    修改图片

--- frontend/src/components/views/home/Banner.vue
+++ frontend/src/components/views/home/Banner.vue
@@ -1,25 +1,15 @@
 <template>
-  <el-carousel 
-  :interval="50000" 
-  autoplay
-  indicator-position="bottom"
-  type="card"
-  :style="{height:'300px',boxShadow: '0 2px 8px rgba(0,0,0,0.1)'}"
-  >
-    <el-carousel-item v-for="(item, i) in banners" :key="i">
-      <img 
-        :src="item.src" 
-        :alt="item.title"
-        :style="{width:'100%',height:'100%',objectFit:'cover'}"
-        />
+  <el-carousel :interval="10000" type="card" height="200px">
+    <el-carousel-item v-for="src in banners" :key="src">
+      <img class="w-full h-full" :src="src" alt="banner"/>
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <script setup lang="ts">
 const banners = [
-  { src: '/banners/image1.png', title: '轮播图1' },
-  { src: '/banners/image2.png', title: '轮播图2' },
-  { src: '/banners/image3.png', title: '轮播图3' },
+  'banners/image1.png',
+  'banners/image2.png',
+  'banners/image3.png',
 ]
 </script>
