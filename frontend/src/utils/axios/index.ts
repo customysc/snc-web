3623895a924f01d492d9356f08235ec5184eb8d8
不留 <weinibuliu@outlook.com>
2026-03-26 21:30:30 +0800

    fix: `127.0.0.1` -> `localhost` (#20)

--- frontend/src/utils/axios/index.ts
+++ frontend/src/utils/axios/index.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1', // 更改为后端API地址
-    timeout: 2000,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: "http://localhost:8000/api/v1", // 更改为后端API地址
+  timeout: 2000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default http;
